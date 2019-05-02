6328eb3e26ddde51b1080eeded6c85152846f820
unknown <arnabchakraborty771@gmail.com>
2019-05-02 09:45:21 +0530

    Removed 'Close Window' function

--- src/history.js
+++ src/history.js
@@ -1,17 +1,10 @@
 const electron = require('electron')
 const fs = require('fs')
 const path = require('path')
-const remote = electron.remote
 
-const closeBtn = document.getElementById('closeBtn');
-
-closeBtn.addEventListener('click', function(event) {
-    var window = remote.getCurrentWindow();
-    window.close();
-})
 const userAppDataPath = (electron.app || electron.remote.app).getPath('userData');
-const animePentaScorePath = path.join(userAppDataPath, 'Anime PentaScore')
-const scoreHistoryPath = path.join(animePentaScorePath, 'Score History');
+	
+const scoreHistoryPath = path.join(userAppDataPath, 'Score History');
 
 fs.readFile(scoreHistoryPath, 'utf8', function (err , history) {
     if (err) throw err;
@@ -23,7 +16,7 @@ deleteBtn.addEventListener('click', function (event) {
     fs.unlinkSync(scoreHistoryPath);
     const notification = {
         title: 'History Cleared',
-        body: 'Please restart the history window to check that history has been cleared'
+        body: 'Please restart the history window should you wish to check if history has been cleared.'
     }
     new window.Notification(notification.title, notification);
 })
